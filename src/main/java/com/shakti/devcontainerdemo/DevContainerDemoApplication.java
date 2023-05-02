9dfc6656c3ab8fed1dada39291f3814e192a6a9b
SHAKTI_GOYAL <shakti_goyal@dell.com>
2023-05-02 21:37:22 +0530

    initial commit

---
+++ src/main/java/com/shakti/devcontainerdemo/DevContainerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.shakti.devcontainerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DevContainerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevContainerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shakti/devcontainerdemo/DevContainerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shakti.devcontainerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevContainerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
